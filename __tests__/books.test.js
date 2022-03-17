3eda4291f4942a67fcb6a78ce9e11e9f2972886d
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-03-17 15:45:26 -0700

    get for books complete

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Book = require('../lib/models/Book');
 
 describe('books routes', () => {
   beforeEach(() => {
@@ -22,5 +23,12 @@ describe('books routes', () => {
     const res = await request(app).post('/api/v1/books').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of books', async () => {
+    const expected = await Book.findAll();
+    const res = await request(app).get('/api/v1/books');
+
+    expect(res.body).toEqual(expected);
   });
 }); 
